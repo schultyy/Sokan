a2a2cb2abf453c60fcfa6230361689fe26e5eed4
Jan Schulte <hello@unexpected-co.de>
2015-08-12 20:01:35 +0000

    read default.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
     println!("sokan");
+    let mut fileResult :std::fs::File;
+    match File::open("default.yaml") {
+        Ok(result) => fileResult = result,
+        Err(error) => {
+            println!("Error occured while accessing default.yaml: {:?}", error);
+            return;
+        }
+    }
+
+    let mut yaml_file = String::new();
+    match fileResult.read_to_string(&mut yaml_file) {
+        Err(error) => println!("fail"),
+        _ => { }
+    }
 }
