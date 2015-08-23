c490fb0ecd9eca6d7bb3896cd92c822f2fa0dd84
Jan Schulte <hello@unexpected-co.de>
2015-08-23 17:44:09 +0200

    Use colors when printing results

--- src/output.rs
+++ src/output.rs
@@ -1,6 +1,8 @@
 use std::process::Output;
 use std::process::ExitStatus;
 use std::fmt::Display;
+extern crate term;
+use std::io::prelude::*;
 
 pub fn print_shellout<T: Display>(thing: &T, shellout: &Output) {
     println!("Executed {}", thing);
@@ -9,12 +11,21 @@ pub fn print_shellout<T: Display>(thing: &T, shellout: &Output) {
 
 fn print_output(shellout: &Output) {
     let exit_status = shellout.status;
+    let mut stdout_terminal = term::stdout().unwrap();
+    let mut stderr_terminal = term::stderr().unwrap();
+    stderr_terminal.fg(term::color::RED).unwrap();
+    stdout_terminal.fg(term::color::GREEN).unwrap();
+
     match exit_status.success() {
         true => {
-            println!("==> {:?}", String::from_utf8_lossy(&shellout.stdout))
+            let stdout_str = format!("==> {:?}", String::from_utf8_lossy(&shellout.stdout));
+            writeln!(stdout_terminal, "{}", stdout_str).unwrap();
         },
         false => {
-            println!("==> {:?}", String::from_utf8_lossy(&shellout.stderr))
+            let stderr_str = format!("==> {:?}", String::from_utf8_lossy(&shellout.stderr));
+            writeln!(stderr_terminal, "{}", stderr_str);
         }
     }
+    stderr_terminal.reset().unwrap();
+    stdout_terminal.reset().unwrap();
 }
