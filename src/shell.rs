006e4c2dde12e6e9dbc9dba2efafaf9c44fa33d0
Jan Schulte <hello@unexpected-co.de>
2015-08-27 20:23:46 +0200

    remove support for commands

--- src/shell.rs
+++ src/shell.rs
@@ -1,5 +1,5 @@
 use std::process::{Command, Output};
-use command;
+use std::io::Error;
 use configuration;
 use output;
 use file;
@@ -7,24 +7,12 @@ use file;
 pub fn provision(configuration: &configuration::Configuration) -> i32 {
 
     let mut packages = Vec::new();
-    let mut commands = Vec::new();
 
     packages.extend(configuration.packages.iter().cloned());
-    commands.extend(configuration.commands.clone());
 
-    commands.reverse();
     packages.reverse();
 
     let mut exit_codes = Vec::new();
-
-    while let Some(cmd) = commands.pop() {
-        let shellout = run(&cmd);
-        output::print_shellout::<command::Command>(&cmd, &shellout);
-        let exit_status = shellout.status.clone();
-        if exit_status.success() == false {
-            exit_codes.push(exit_status.code().unwrap());
-        }
-    }
 
     let package_exit_codes = install_packages(&packages);
     exit_codes.extend(&package_exit_codes[..]);
@@ -91,12 +79,4 @@ fn install_package(package: &String) -> Output {
         .args(&vec!["yum", "install", "-y", package])
         .output()
         .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) })
-}
-
-fn run(command: &command::Command) -> Output {
-    let output_handle = Command::new(command.command.to_string())
-        .args(&command.args)
-        .output()
-        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
-    output_handle
 }
